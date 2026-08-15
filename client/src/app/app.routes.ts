f6ec01bcd059193d7d271c666d905a052c0d4ece
Ramzyayman <Ramzyayman9@gmail.com>
2026-08-15 22:01:25 +0300

    Project Intialized - Home Page, Header, and Footer created

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Categories } from './features/categories/categories';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'categories',
+        component: Categories
+    }
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Header,
+    Footer,
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
